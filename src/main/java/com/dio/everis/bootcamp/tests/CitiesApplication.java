35d27098e4af8672de864cf8d70026ef8e8204c6
Alexandre Tannus <alexandretannus@gmail.com>
2021-07-26 09:23:17 -0300

    Project start. Database config and migrations

---
+++ src/main/java/com/dio/everis/bootcamp/tests/CitiesApplication.java
@@ -0,0 +1,13 @@
+package com.dio.everis.bootcamp.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesApplication.class, args);
+	}
+
+}
